3a3680cb068c816a853401c0686ee4db2ee46c9b
Shih-Ting, Yuan <shihtiy.tw@gmail.com>
2024-11-01 09:13:24 +0000

    feat(fargate): init implement opensearch logging for fargate

--- labs/scenario/fargate-nginx-logging/opensearch/setup.sh
+++ labs/scenario/fargate-nginx-logging/opensearch/setup.sh
@@ -9,6 +9,19 @@ POLICY_URL=https://raw.githubusercontent.com/aws-samples/amazon-eks-fluent-loggi
 
 ROLE_NAME="$(eksctl get fargateprofile --cluster "$EKS_CLUSTER_NAME" --region "$AWS_REGION" --output json | jq -r '.[0].podExecutionRoleARN | split("/") | last')"
 
+echo -e "${YELLOW}Step 3: Creating IAM policy...${NC}"
+if ! aws iam list-policies --query "Policies[].[PolicyName,UpdateDate]" --output text | grep "$IAM_POLICY_NAME"; then
+  if aws iam create-policy --policy-name "$IAM_POLICY_NAME" --policy-document file://policy.json; then
+    echo -e "${GREEN}IAM policy created successfully.\n${NC}"
+  else
+    echo -e "${RED}Failed to create IAM policy.${NC}"
+    exit 0
+  fi
+else
+  echo -e "${GREEN}IAM policy already exists.\n${NC}"
+fi
+
+
 curl -O "$POLICY_URL"
 
 aws iam create-policy --policy-name "$POLICY_NAME" --policy-document file://permissions.json
@@ -21,8 +34,12 @@ aws iam attach-role-policy \
 export TF_VAR_EKSCLUSTER=$EKS_CLUSTER_NAME
 terraform apply
 
+# TODO: configmap for opensearch logging
+
 # export INDEX
 # export HOST
 # export TYPE
 # export REGION
 # envsubst < k8s-configmap-logging-opensearch.yaml | kubectl apply -f -
+
+# TODO: add fargate role to the opensearch access
