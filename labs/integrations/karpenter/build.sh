c90a871bfb67039f0de8b7738c2aae7d4543b620
Shih-Ting, Yuan <shihtiy.tw@gmail.com>
2024-11-04 16:37:57 +0000

    chore(lab): hardcode karpenter version for now

--- labs/integrations/karpenter/build.sh
+++ labs/integrations/karpenter/build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Usage:
-#   ./build.sh                     - List all available chart versions
-#   ./build.sh latest              - Install/update to the latest chart version
+# ./build.sh                     - List all available chart versions
+# ./build.sh latest              - Install/update to the latest chart version
 # ./build.sh <chart version> <app version>  Install/update to a specific chart version
-# Example: ./build.sh 1.8.3 v2.8.3
+# Example: ./build.sh 1.0.7 1.0.7
 #
 
 # Colors for pretty output
@@ -52,69 +52,74 @@ print_info "Service Account Name" "$SERVICE_ACCOUNT_NAME"
 echo -e "\n${YELLOW}======================================${NC}\n"
 
 # Step 1: Add EKS Charts repo
-echo -e "${YELLOW}Step 1: Adding EKS Charts repo...${NC}"
-if ! helm repo list | grep -q 'karpenter'; then
-  if helm repo add karpenter https://charts.karpenter.sh/; then
-    echo -e "${GREEN}Karpenter Charts repo added successfully.\n${NC}"
-  else
-    echo -e "${RED}Failed to add Karpenter Charts repo.\n${NC}"
-  fi
-else
-  echo -e "${GREEN}Karpenter Charts repo already exists.\n${NC}"
-fi
-
-# Step 2: Update EKS Charts repo
-echo -e "${YELLOW}Step 2: Updating Karpenter Charts repo...${NC}"
-if helm repo update karpenter; then
-  echo -e "${GREEN}Karpenter Charts repo updated successfully.\n${NC}"
-else
-  echo -e "${RED}Failed to update Karpenter Charts repo.\n${NC}"
-  exit 0
-fi
-
-# Function to get the latest chart version
-get_chart_versions() {
-  # Define color codes using ANSI escape sequences
-  versions=$(helm search repo karpenter/karpenter --versions --output json |
-           jq -r '.[] | "\(.version),\(.app_version)"' |
-           head -n 10)
-  echo -e "${BLUE}Available versions for aws-load-balancer-controller:${NC}"
-  echo -e "${GREEN}CHART VERSION   APP VERSION${NC}"
-
-  while IFS=',' read -r chart_version app_version; do
-      printf "%-15s %s\n" "$chart_version" "$app_version"
-  done <<< "$versions"
-}
-
+#
+# TODO: need to figure how to find the karpenter version
+#
 # $ helm repo add karpenter https://charts.karpenter.sh/
 # "karpenter" has been added to your repositories
 #
 # $ helm search repo karpenter
 # NAME                    CHART VERSION   APP VERSION     DESCRIPTION
 # karpenter/karpenter     0.16.3          0.16.3          A Helm chart for Karpenter, an open-source node...
-# TODO: need to figure how to find the karpenter version
+#
+# echo -e "${YELLOW}Step 1: Adding EKS Charts repo...${NC}"
+# if ! helm repo list | grep -q 'karpenter'; then
+#   if helm repo add karpenter https://charts.karpenter.sh/; then
+#     echo -e "${GREEN}Karpenter Charts repo added successfully.\n${NC}"
+#   else
+#     echo -e "${RED}Failed to add Karpenter Charts repo.\n${NC}"
+#   fi
+# else
+#   echo -e "${GREEN}Karpenter Charts repo already exists.\n${NC}"
+# fi
+#
+# # Step 2: Update EKS Charts repo
+# echo -e "${YELLOW}Step 2: Updating Karpenter Charts repo...${NC}"
+# if helm repo update karpenter; then
+#   echo -e "${GREEN}Karpenter Charts repo updated successfully.\n${NC}"
+# else
+#   echo -e "${RED}Failed to update Karpenter Charts repo.\n${NC}"
+#   exit 0
+# fi
 
-get_latest_chart_version(){
-  # Fetch the latest chart version
-  version=$(helm search repo eks/aws-load-balancer-controller --versions --output json )
-  CHART_VERSION=$(echo "$version"| jq -r '.[0].version')
-
-  # If you also want the corresponding app version:
-  APP_VERSION=$(echo "$version" | jq -r '.[0].app_version')
-}
+# Function to get the latest chart version
+# get_chart_versions() {
+#   # Define color codes using ANSI escape sequences
+#   versions=$(helm search repo karpenter/karpenter --versions --output json |
+#            jq -r '.[] | "\(.version),\(.app_version)"' |
+#            head -n 10)
+#   echo -e "${BLUE}Available versions for aws-load-balancer-controller:${NC}"
+#   echo -e "${GREEN}CHART VERSION   APP VERSION${NC}"
+#
+#   while IFS=',' read -r chart_version app_version; do
+#       printf "%-15s %s\n" "$chart_version" "$app_version"
+#   done <<< "$versions"
+# }
+#
+# get_latest_chart_version(){
+#   # Fetch the latest chart version
+#   version=$(helm search repo eks/aws-load-balancer-controller --versions --output json )
+#   CHART_VERSION=$(echo "$version"| jq -r '.[0].version')
+#
+#   # If you also want the corresponding app version:
+#   APP_VERSION=$(echo "$version" | jq -r '.[0].app_version')
+# }
 
 
 # If no parameter is provided, print all available versions and exit
-if [ $# -eq 0 ]; then
-  echo -e "${YELLOW}Step 3: Listing all available helm chart versions for EKS ${CLUSTER_VERSION}:${NC}"
-  CHART_VERSIONS=$(get_chart_versions)
-  echo "$CHART_VERSIONS"
-  exit 0
-fi
+# if [ $# -eq 0 ]; then
+#   echo -e "${YELLOW}Step 3: Listing all available helm chart versions for EKS ${CLUSTER_VERSION}:${NC}"
+#   CHART_VERSIONS=$(get_chart_versions)
+#   echo "$CHART_VERSIONS"
+#   exit 0
+# fi
 
 # If parameter is 'latest', get the latest version
 if [ "$1" = "latest" ]; then
-  get_latest_chart_version
+  # get_latest_chart_version
+  # TODO: rmeove hardcode version
+  APP_VERSION="1.0.7"
+  CHART_VERSION="1.0.7"
   echo -e "${GREEN}Using latest helm chart version: ${CHART_VERSION}${NC}"
   echo -e "${GREEN}Using latest helm app version: ${APP_VERSION}\n${NC}"
 else
