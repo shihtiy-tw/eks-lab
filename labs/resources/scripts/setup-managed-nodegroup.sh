61c963058b6281300636e0a05d2ccad6c7edc60e
Shih-Ting, Yuan <shihtiy.tw@gmail.com>
2024-12-16 18:21:13 +0000

    chore(lab): try to setup al2023 custom ami
    still need to overcome the userdata for node config in custom ami

--- labs/resources/scripts/setup-managed-nodegroup.sh
+++ labs/resources/scripts/setup-managed-nodegroup.sh
@@ -43,6 +43,27 @@ printf "${GREEN}%-20s${NC}%s\n" "Nodegroup Size:" "$NODEGROUP_SIZE"
 printf "${GREEN}%-20s${NC}%s\n" "Nodegroup YAML File:" "$NODEGROUP_FILE"
 printf "${BLUE}--------------------------------${NC}\n"
 
+if [[ $NODEGROUP_CONFIG = "custom-ami" ]]; then
+  # custom ami name: "eks-lab-amazon-eks-arm64-1.29-20241023145809"
+  export CUSTOM_AMI=$(aws ec2 describe-images \
+    --filters "Name=name,Values=eks-lab*-${CLUSTER_VERSION}-*" "Name=state,Values=available" \
+    --query 'Images[*].ImageId' --output text)
+
+  cat "$PWD/nodegroups/eksctl-managed-nodegroup-$NODEGROUP_CONFIG".yaml | envsubst '${EKS_CLUSTER_NAME},${CLUSTER_VERSION},${EKS_CLUSTER_REGION},${AZ_ARRAY},${NODEGROUP_CONFIG},${NODEGROUP_SIZE},${CUSTOM_AMI}' > "$NODEGROUP_FILE"
+
+elif [[ $NODEGROUP_CONFIG = "al2023-custom-ami" ]]; then
+  # custom ami name: "eks-lab-amazon-eks-arm64-1.29-20241023145809"
+  export CUSTOM_AMI=$(aws ssm get-parameter --name /aws/service/eks/optimized-ami/"$CLUSTER_VERSION"/amazon-linux-2023/x86_64/standard/recommended/image_id \
+    --region "$EKS_CLUSTER_REGION" --query "Parameter.Value" --output text)
+
+  cat "$PWD/nodegroups/eksctl-managed-nodegroup-$NODEGROUP_CONFIG".yaml | envsubst '${EKS_CLUSTER_NAME},${CLUSTER_VERSION},${EKS_CLUSTER_REGION},${AZ_ARRAY},${NODEGROUP_CONFIG},${NODEGROUP_SIZE},${CUSTOM_AMI}' > "$NODEGROUP_FILE"
+
+else
+  cat "$PWD/nodegroups/eksctl-managed-nodegroup-$NODEGROUP_CONFIG".yaml | envsubst '${EKS_CLUSTER_NAME},${CLUSTER_VERSION},${EKS_CLUSTER_REGION},${AZ_ARRAY},${NODEGROUP_CONFIG},${NODEGROUP_SIZE}' > "$NODEGROUP_FILE"
+fi
+
+cat "$PWD/nodegroups/eksctl-managed-nodegroup-$NODEGROUP_CONFIG".yaml | envsubst '${EKS_CLUSTER_NAME},${CLUSTER_VERSION},${EKS_CLUSTER_REGION},${AZ_ARRAY},${NODEGROUP_CONFIG},${NODEGROUP_SIZE}' > "$NODEGROUP_FILE"
+# envsubst '${EKS_CLUSTER_NAME},${CLUSTER_VERSION},${EKS_CLUSTER_REGION},${AZ_ARRAY},${NODEGROUP_CONFIG},${NODEGROUP_SIZE}' < $(pwd)/nodegroups/managed-nodegroup-${NODEGROUP_CONFIG}.yaml
 
 # Capture both stdout and stderr, and store the exit status
 printf "${BLUE}Validating the template...${NC}\n"
